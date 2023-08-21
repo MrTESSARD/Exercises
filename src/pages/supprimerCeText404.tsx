b072ad1a715c4e2e109dde2f1f80ea37e7b654a2
MrTESSARD <dimitri@tessard.pro>
2023-08-21 09:35:27 +0200

    new file:   src/pages/supprimerCeText404.tsx

---
+++ src/pages/supprimerCeText404.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+      <div>
+        <h1>Erreur 404 - Page non trouvée</h1>
+        <p>Désolé, la page que vous cherchez n'existe pas.</p>
+      </div>
+    );
+  }
+  
+  export default NotFound;
